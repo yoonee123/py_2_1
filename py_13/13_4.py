7f488b1a5353987627beca26b3a942ceb2bedb37
yoonee123 <78791337+yoonee123@users.noreply.github.com>
2024-06-04 16:24:43 +0900

    Delete py_13 directory

--- py_13/13_4.py
+++
@@ -1,9 +0,0 @@
-#Button(window, text="버튼에 표시될 글자", 속성, command=명령어실행및함수호출)
-from tkinter import*
-window=Tk()
-window.title("윈도우 창 연습")
-window.geometry("400x500")
-
-button1 = Button(window, text="파이썬 종료", fg="red", command=quit)
-button1.pack()
-window.mainloop()
